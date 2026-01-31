f7d27cdb12b488d9baa96fd96d8866a65a295794
KTaylorNo <kristofert@outlook.com>
2026-01-31 16:30:29 +0100

    Add script to download dataset from EGA

---
+++ analyses/data_download.sh
@@ -0,0 +1,6 @@
+# Download dataset from the European Genome-Phenome Archive (EGA)
+
+pyega3 -cf </Path/To/Credentials_File> \
+       -c 15 \
+       fetch EGAD00001008644 \
+       --output-dir </Path/To/OutputDirectory>
